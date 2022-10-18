edbabc960e72012930e1b0ab77fd776709f8215f
Moisés Alexandre Monteiro Araujo <97893793+MoisesMonter@users.noreply.github.com>
2022-10-18 17:30:47 -0300

    Add files via upload

--- mainscript.js
+++ mainscript.js
@@ -1,5 +1,35 @@
-function Sorteio(L1,L2){
-    const as = 'informações';
-    console.log(listalimpa1)
-    document.getElementById("info").innerHTML = as;
+function Sorteio(){
+    let input1 = document.querySelector("#Lista1");
+    var l1 = input1.value;
+    let input2 = document.querySelector("#Lista2");
+    var l2 = input2.value;
+
+    var list1 = l1.split(",");
+
+    var list2 = l2.split(",");
+    var result = ""
+    if (l1 == "" || l2 == "")
+    {
+       alert("Opa... tem Lista vasia aí irmão") 
+    }
+    else
+    {
+        resultado.innerHTML = l1.value;
+        if (list1.length == list2.length)
+        {
+           
+            for (let i = 0; i <= list1.length; i++)
+            {
+                result += list1[i] + "\n" + list2[i]
+            }
+             
+        }
+        else
+        {
+            alert("As listas devem ter o mesmo número de informações")
+        }
+    }
+    
+    resultado.innerHTML = list1
 }
+    //resultado.innerHTML = string
